6256d5a9bf00c4bab4bdac5c442533c4ad33b4d5
mathiasjor911 <mj@mathiasjorgensen.dk>
2020-08-20 03:05:17 +0200

    tekst til about

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,15 +14,24 @@ export default function About() {
       <div className="aboutgrid">
         <div className="about-content">
           <p>
-            My name is Mathias Jørgensen, I’m currently{" "}
+            My name is Mathias Jørgensen and I’m currently{" "}
             {moment().diff("11-Sep-1998", "years")} years old.
-            <br /> <br />I study Web Development at Københavns Erhvervsakdemi
+            <br /> <br />
+            I'm currently studying Web Development at Københavns Erhvervsakdemi
             (KEA).
             <br />
-            <br />A good fucking composition is the result of a hierarchy
-            consisting of clearly contrasting elements set with distinct
-            alignments containing irregular intervals of negative space.
-            Creativity is a fucking work-ethic.
+            <br />I am a young and aspiring front-end developer, who strives to
+            learn more everyday. Right now I'm very interested in React and the
+            JAMstack way of buidling websites.
+            <br />
+            <br />
+            Besides Front-End development I like to stay up-to-date on the
+            newest design trends and tools - and I frequently sketch out
+            projects in Figma, both for school or personal use.
+            <br />
+            <br />
+            In my spare time I like to hang out with friends and shoot 35mm
+            photos
           </p>
           <div className="interrests">
             <h3>Interrests</h3>
